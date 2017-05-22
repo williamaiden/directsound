e38144d0110b8cff6d574adf1e0f55a20ffd2279
williamaiden <williamaiden@qq.com>
2017-05-22 21:18:55 +0800

    Create Config.h

---
+++ Config.h
@@ -0,0 +1,9 @@
+#ifndef _WIN32_CONFIG_H
+#define _WIN32_CONFIG_H
+
+#define MEDIA_BITS_PER_SAMPLE_DEFAULT  16
+#define MEDIA_CHANNELS_DEFAULT         1
+#define MEDIA_RATE_DEFAULT             8000
+#define MEDIA_PTIME_DEFAULT            60
+
+#endif
